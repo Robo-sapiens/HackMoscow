494df9feefc02b22b8b41fe6b69f4223d7ca6f19
anton_ilin_1999@mail.ru <anton_ilin_1999@mail.ru>
2019-10-26 03:58:25 +0300

    example

--- demon/src/daemon.c
+++ demon/src/daemon.c
@@ -5,7 +5,31 @@
 #include "daemon.h"
 #include <stdio.h>
 #include <bass.h>
+#include <stdbool.h>
+#include <string.h>
 
 void daemon_func() {
-    printf("asd");
+//    DWORD chan;
+
+    if (!BASS_RecordInit(-1)) {
+        printf("error");
+        return;
+    }
+//    chan = BASS_RecordStart(44100, 2, 0, 0, 0); // Recording Channel
+//    if ((!r_init) || (!chan)) {
+//        BASS_Free();
+//    }
+    printf("her\n");
+
+    char *str = "";
+    for (int i = 0; str = BASS_RecordInit(i); i++) {
+        printf("%s\n", str);
+    }
+//    printf("f\n");
+//    while (!str) {
+//        printf("%s\n", str);
+//        ++a;
+//        str = strdup(BASS_RecordGetInputName(a));
+//    }
+    printf("f\n");
 }
