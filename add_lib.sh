c717acde3be67d4778cf68bbd07410dc3668cc48
anton_ilin_1999@mail.ru <anton_ilin_1999@mail.ru>
2019-10-26 23:08:56 +0300

    add lib and test_file

---
+++ add_lib.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install libcap-dev
+sudo apt-get install libsndfile-dev
+git clone https://gitlab.freedesktop.org/pulseaudio/pulseaudio.git 
+cd pulseaudio
+./bootstrap.sh
+make
+sudo make install
+sudo ldconfig
+pulseaudio --start -D
+
+
--- demon/src/daemon.c
+++ demon/src/daemon.c
@@ -4,67 +4,50 @@
 
 #include "daemon.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <bass.h>
 #include <stdbool.h>
 #include <string.h>
 
+DWORD output;
+DWORD input;
+int prebuf;
+
+BOOL CALLBACK RecordProc(HRECORD handle, const void *buffer, DWORD length, void *user) {
+    BASS_StreamPutData(output, buffer, length);
+    printf("%p", buffer);
+    if (prebuf > 0) {
+        prebuf -= length;
+        if (prebuf <= 0) {
+            BASS_ChannelPlay(output, FALSE);
+        }
+    }
+    return TRUE;
+}
+
 void daemon_func() {
-    BASS_DEVICEINFO di;
-    int a;
-    printf("Output Devices\n");
-    for (a=1;BASS_GetDeviceInfo(a,&di);a++) {
-        printf("%d: ",a);
-        DisplayDeviceInfo(&di);
+    FILE *file = fopen("file.wav", "a");
+    if (HIWORD(BASS_GetVersion()) != BASSVERSION) {
+        printf("An incorrect version of BASS was loaded");
+        return;
     }
-    printf("\nInput Devices\n");
-    for (a=0;BASS_RecordGetDeviceInfo(a,&di);a++) {
-        printf("%d: ",a);
-        DisplayDeviceInfo(&di);
+
+    // initalize default recording device^M
+    if (!BASS_RecordInit(-1)) {
+        printf("Can't initialize recording device");
+        return;
     }
+    // initialize default output device^M
+    if (!BASS_Init(-1, 44100, 0, NULL, NULL)) {
+        printf("Can't initialize output device");
+    }
+    output = BASS_StreamCreate(44100, 1, 0, STREAMPROC_PUSH, 0);
+    BASS_INFO info;
+    BASS_GetInfo(&info);
+    prebuf = BASS_ChannelSeconds2Bytes(output, info.minbuf / 1000.0);
+    printf("%d\n", prebuf);
+    input = BASS_RecordStart(4410, 1, MAKELONG(0, 10), RecordProc, 0);
 
 }
 
-void DisplayDeviceInfo(BASS_DEVICEINFO *di) {
-    printf("%s\n\tdriver: %s\n\ttype: ",di->name,di->driver);
-    switch (di->flags&BASS_DEVICE_TYPE_MASK) {
-        case BASS_DEVICE_TYPE_NETWORK:
-            printf("Remote Network");
-            break;
-        case BASS_DEVICE_TYPE_SPEAKERS:
-            printf("Speakers");
-            break;
-        case BASS_DEVICE_TYPE_LINE:
-            printf("Line");
-            break;
-        case BASS_DEVICE_TYPE_HEADPHONES:
-            printf("Headphones");
-            break;
-        case BASS_DEVICE_TYPE_MICROPHONE:
-            printf("Microphone");
-            break;
-        case BASS_DEVICE_TYPE_HEADSET:
-            printf("Headset");
-            break;
-        case BASS_DEVICE_TYPE_HANDSET:
-            printf("Handset");
-            break;
-        case BASS_DEVICE_TYPE_DIGITAL:
-            printf("Digital");
-            break;
-        case BASS_DEVICE_TYPE_SPDIF:
-            printf("SPDIF");
-            break;
-        case BASS_DEVICE_TYPE_HDMI:
-            printf("HDMI");
-            break;
-        case BASS_DEVICE_TYPE_DISPLAYPORT:
-            printf("DisplayPort");
-            break;
-        default:
-            printf("Unknown");
-    }
-    printf("\n\tflags:");
-    if (di->flags&BASS_DEVICE_ENABLED) printf(" enabled");
-    if (di->flags&BASS_DEVICE_DEFAULT) printf(" default");
-    printf(" (%x)\n",di->flags);
-}
+
